808c9b3f38eaa3cc1c1426a548b15ed90e469a76
Cristi Miloiu <miloiuc4@gmail.com>
2024-05-09 14:18:51 +0300

    update 09-05

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,11 @@
-import {GenezioDeploy} from "@genezio/types";
 import {LlmService} from "./llm";
 import {execSync} from "node:child_process";
 import * as fs from "fs";
 import path from "path";
+import dotenv from "dotenv";
 
-@GenezioDeploy()
+dotenv.config();
+
 export class BackendService {
   llmService: LlmService;
 
@@ -14,7 +15,10 @@ export class BackendService {
 
   async createTheProject(classInfo: string, functionsListStr: string, question: string, dbOrm: string, dbUrl: string) {
     try {
-      const {classCode, className} = await this.llmService.ask(classInfo, functionsListStr, question, dbOrm, dbUrl);
+      const {
+        classCode,
+        className
+      } = await this.llmService.askForClassCode(classInfo, functionsListStr, question, dbOrm, dbUrl);
       const tmpDirPath = path.join(process.cwd(), "tmp");
 
       if (!fs.existsSync(tmpDirPath)) {
@@ -22,36 +26,62 @@ export class BackendService {
       }
 
       const command = `genezio create backend --backend=onboarding-ts --name=${classInfo} --region=eu-central-1`;
-      console.log("Running command: ", command);
+      console.log("Creating the project...");
       execSync(command, {cwd: tmpDirPath});
       const pathFolder = path.join(tmpDirPath, classInfo);
       fs.writeFileSync(
         `${pathFolder}/${className}.ts`,
         classCode
       )
-
+      fs.unlinkSync(`${pathFolder}/genezio.yaml`);
+      fs.unlinkSync(`${pathFolder}/.genezioignore`);
+      fs.unlinkSync(`${pathFolder}/.eslintrc.js`);
+      fs.rm(`${pathFolder}/node_modules/@genezio`, {recursive: true}, (err) => {
+        if (err) {
+          console.error(err);
+        }
+      });
+      const packageJson = JSON.parse(fs.readFileSync(`${
+        pathFolder
+      }/package.json`, "utf8"));
+      delete packageJson.dependencies["@genezio/types"];
+      fs.writeFileSync(
+        `${pathFolder}/package.json`,
+        JSON.stringify(packageJson, null, 2)
+      );
       while (!fs.existsSync(`${pathFolder}/${className}.ts`)) {
         console.log("Waiting for file to be created...", `${pathFolder}/${className}.ts`);
-        await new Promise(resolve => setTimeout(resolve, 1000));
+        await new Promise(resolve => setTimeout(resolve, 100));
       }
 
       const imports = classCode.match(/import.*?;/g);
       const regexPackage = /['"]([^'"]*)['"]/;
 
-      const importsSave= [];
+      const importsSave = [];
       if (imports) {
         for (const imp of imports) {
           const importName = imp.match(regexPackage)![1];
           importsSave.push(importName);
-          execSync(`npm install ${importName}`, {cwd: pathFolder});
         }
       }
 
-      if(importsSave.includes("sequelize")) {
-        execSync(`npm install pg`, {cwd: pathFolder});
-        execSync(`npm install pg-hstore`, {cwd: pathFolder});
+      if (importsSave.includes("sequelize")) {
+        importsSave.push("pg");
+        importsSave.push("pg-hstore");
       }
 
+      if (importsSave.includes("nodemailer")) {
+        execSync(`npm i --save-dev @types/nodemailer`, {cwd: pathFolder});
+      }
+
+      const importsSaveUnique = [...new Set(importsSave)];
+      console.log(importsSaveUnique)
+
+      for (const imp of importsSaveUnique) {
+        execSync(`npm install ${imp}`, {cwd: pathFolder});
+      }
+
+      console.log("Project created successfully.");
       return 0;
     } catch (error) {
       console.error(error)
@@ -59,3 +89,27 @@ export class BackendService {
     }
   }
 }
+
+(async () => {
+  console.log("Create the project..");
+  const backendService = new BackendService();
+
+  // Create the project Crud APP
+  await backendService.createTheProject(
+    "UserService",
+    "create, delete, update, read",
+    "Asigura-te ca returnezi userul la functia de create. la functia de update la fel, returnezi userul. la functia de delete returnezi da sau nu. la functia de get returnezi userul.",
+    "mongoose",
+    "mongodb+srv://miloiuc4:mPcKjbBivX7Yo7YV@cluster0.2xtg8qs.mongodb.net/");
+
+  //Send mail
+  await backendService.createTheProject(
+    "EmailService",
+    "sendMail",
+    "Create a class that send an email with nodemailer, the function should return a boolean if the email was sent or not.",
+    "",
+    "");
+
+  console.log("Successfully created the project.");
+})();
+
--- server/llm.ts
+++ server/llm.ts
@@ -1,89 +1,139 @@
-import {GenezioDeploy} from "@genezio/types";
-import {LanceDB} from "@langchain/community/vectorstores/lancedb";
-import {ChatPromptTemplate} from "@langchain/core/prompts";
-import {OpenAI, OpenAIEmbeddings} from "@langchain/openai";
-import {connect} from "vectordb";
+import { LanceDB } from "@langchain/community/vectorstores/lancedb";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { OpenAI, OpenAIEmbeddings } from "@langchain/openai";
+import { connect } from "vectordb";
 import {
   RunnableLambda,
   RunnableMap,
   RunnablePassthrough,
 } from "@langchain/core/runnables";
-import {StringOutputParser} from "@langchain/core/output_parsers";
+import { StringOutputParser } from "@langchain/core/output_parsers";
 
-@GenezioDeploy()
 export class LlmService {
-  constructor() {
-  }
+  constructor() {}
 
-  async ask(classInfo: string, functionsListStr: string, question: string, dbOrm: string, dbUrl: string): Promise<{
-    classCode: string,
-    className: string
-  }> {
+  async askForClassCode(
+    classInfo: string,
+    functionsListStr: string,
+    question: string,
+    dbOrm: string,
+    dbUrl: string
+  ): Promise<{ classCode: string; className: string }> {
     try {
       const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
       if (!OPENAI_API_KEY) {
-        throw new Error("You need to provide an OpenAI API key. Go to https://platform.openai.com/account/api-keys to get one.");
+        throw new Error(
+          "You need to provide an OpenAI API key. Go to https://platform.openai.com/account/api-keys to get one."
+        );
       }
 
       const database = "./lancedb";
 
       const model = new OpenAI({
-        modelName: "gpt-3.5-turbo-0125",
+        // modelName: "gpt-3.5-turbo-0125",
+        modelName: "gpt-4",
         openAIApiKey: OPENAI_API_KEY,
-        temperature: 0.5,
-        verbose: true
+        temperature: 0.6,
+        verbose: true,
+        // verbose:false
       });
 
       const db = await connect(database);
-      const table = await db.openTable('vectors')
+      const table = await db.openTable("vectors");
 
-      console.log("Opened table")
-      const vectorStore = new LanceDB(new OpenAIEmbeddings, {table})
+      console.log("Opened table");
+      const vectorStore = new LanceDB(new OpenAIEmbeddings(), { table });
       const retriever = vectorStore.asRetriever(1);
-
-      const prompt = ChatPromptTemplate.fromMessages([
-        [
-          "ai",
-          `You are a an AI assistant that helps developers write code. You have been asked to help a developer write a ${classInfo} class in TypeScript that implements the following functions: ${functionsListStr}.
-         Always rely on the last point because that is actually the user's question. If necesarry, also use your khnowledge. Never use comments.`
-        ], [
-          "human",
+      let prompt;
+      if(dbOrm){
+         prompt = ChatPromptTemplate.fromMessages([
+          [
+            "ai",
+            `As an AI assistant for developers, you're tasked with writing a TypeScript class that implements a ${classInfo} and its functions: ${functionsListStr}.
+          Utilize the ${dbOrm} ORM to connect to the database at ${dbUrl}. Remember to declare types for all variables and parameters, and never use "any" type. Implement the logic for the functions ${functionsListStr} directly within the class.
+          Rely on the last point as the user's question. Feel free to use your knowledge, but avoid using comments.
           `
-Your task is to write a ${classInfo} class in TypeScript that implements the following functions: ${functionsListStr} by following the next steps:
+          ], [
+            "human",
+            `
+          Your assignment is to create a ${classInfo} class in TypeScript, implementing the following functions: ${functionsListStr}. Follow these steps:
+          1. Begin by defining a TypeScript class for ${classInfo}, starting with the line: \`export class <class_name>\`.
 
-1. Write a TypeScript backend class that implements a ${classInfo}. Begin the class declaration must use @GenezioDeploy() decorator from @genezio/types, and declare class by following line \`export class <class_name>\`.
+          2. Ensure that every variable and parameter within the class has a type annotation. Don't use define method.
 
-2. Within the class, ensure that you utilize type annotations for every variable and parameter.
+          3. Use the ${dbOrm} ORM to connect to the specified database at ${dbUrl}. If necessary, define database models within the same file as your class.
 
-3. Utilize the ${dbOrm} ORM to establish a connection to a database. Connect to the database using the following URL: ${dbUrl}. Create the models for the database as needed in the same file as your class.
+          4. Incorporate any required npm packages to enhance your class's functionality. Avoid using external files.
+          Never use "any" type and "useNewUrlParser: true, useUnifiedTopology: true" for the mongoose connection.
 
-4. Use any necessary npmjs packages for your implementation. Feel free to leverage these packages as needed to enhance the functionality of your class. Never use any external files.
+          5. Provide implementations for all specified methods, such as ${functionsListStr}. Don't use interfaces for input parameters, use primitive types.
 
-5. Your class should include specific methods such as ${functionsListStr}. Ensure that you provide the implementation for each of these methods.
+          6. Format your source code within triple backticks (\`\`\`) when presenting it. Exclude any additional text outside the code.
+  
+          7. Always when you declare a class, always use the "export" keyword.
+          
+          8. Always when you declare a type, always use the "export" keyword.
+          
+          9. Always when you declare a variable outside the class, always use the "export" keyword.
+          
+          10. Never use "any" type and "useNewUrlParser: true, useUnifiedTopology: true" for the mongoose connection.
+ 
+          11. For declare a model, this is the sintax "export const TaskModel = mongoose.models.Task || mongoose.model("Task", taskSchema);
+                    
+          12. For error handling just console.log the error. don't use "error.message" or "error.stack" because error is unknown.
+          
+          13. After you implement the class, implement outside the class a test function that will test the class.   
+          
+          14. Don't use "findByIdAndRemove" method from mongoose, instead use "findByIdAndDelete".
+          
+          15. ${question}`
+          ]
+        ]);
+      }
+      else{
+        prompt = ChatPromptTemplate.fromMessages([
+          [
+            "ai",
+            `As an AI assistant for developers, you're tasked with writing a TypeScript class that implements a ${classInfo} and its functions: ${functionsListStr}.
+          Remember to declare types for all variables and parameters, and never use "any" type. Implement the logic for the functions ${functionsListStr} directly within the class.
+          Rely on the last point as the user's question. Feel free to use your knowledge, but avoid using comments.
+          `
+          ], [
+            "human",
+            `
+          Your assignment is to create a ${classInfo} class in TypeScript, implementing the following functions: ${functionsListStr}. Follow these steps:
+          1. Begin by defining a TypeScript class for ${classInfo}, starting with the line: \`export class <class_name>\`.
 
-6. When presenting your source code, make sure to format it within triple backticks \`\`\`. This formatting helps maintain clarity and readability. Include only the source code itself within the triple backticks, without adding any additional text.
+          2. Ensure that every variable and parameter within the class has a type annotation. Don't use define method.
 
-7. Check the code for any errors or bugs. Make sure that the code is clean and follows best practices. 
+          3. Incorporate any required npm packages to enhance your class's functionality. Avoid using external files.
+          Never use "any" type.
 
-8. If you use database, make sure to init the database tables at the top.
+          4. Provide implementations for all specified methods, such as ${functionsListStr}. Don't use interfaces for input parameters, use primitive types.
 
-9. Don't create initializer example or usage example. Just the class code.
+          5. Format your source code within triple backticks (\`\`\`) when presenting it. Exclude any additional text outside the code.
+  
+          6. Always when you declare a class, always use the "export" keyword.
+          
+          7. Always when you declare a type, always use the "export" keyword.
+          
+          8. Always when you declare a variable outside the class, always use the "export" keyword.
+            
+          9. After you implement the class, implement outside the class a test function that will test the class.   
+          
+          10. all variables must have a type, and all functions must have a return type.
+          
+          11. For error handling just console.log the error. don't use "error.message" or "error.stack" because error is unknown.
+         
+              
+          12. Be careful at eslint errors, don't ignore them. Always use the variables that you declare.
+          
+          13. ${question}`
+          ]
+        ]);
+      }
 
-10. Use all variable that you declared or remove them because eslint will throw an error.
-
-11. Always implement logic methods.
-
-12. For model database init them at the top of the class and outside the class.
-
-13. Never use comments always implement logic methods.
-
-14. At method declaration always use async keyword.
-
-15. ${question}`
-
-        ]
-      ]);
 
       const outputParser = new StringOutputParser();
 
@@ -91,28 +141,27 @@ Your task is to write a ${classInfo} class in TypeScript that implements the fol
         context: new RunnableLambda({
           func: (input: string) =>
             retriever.invoke(input).then((response) => response[0].pageContent),
-        }).withConfig({runName: "contextRetriever"}),
+        }).withConfig({ runName: "contextRetriever" }),
         question: new RunnablePassthrough(),
       });
 
-      const chain = setupAndRetrieval.pipe(prompt).pipe(model).pipe(outputParser)
+      const chain = setupAndRetrieval.pipe(prompt).pipe(model).pipe(outputParser);
 
-      console.log("Ready to invoke")
+      console.log("Ready to invoke");
       const response = await chain.invoke(prompt.toString());
-      console.log("Invoked successfully")
+      console.log("Invoked successfully");
 
-      console.log("Answer:", response)
+      console.log("Answer:", response);
 
       let sourceCode = response.split("```")[1];
       const firstLine = sourceCode.split("\n")[0];
       if (!firstLine.startsWith("import")) {
         sourceCode = sourceCode.split("\n").slice(1).join("\n");
       }
-      // get className from export class <class_name>
-      const className = sourceCode.split("export class ")[1].split(" ")[0];
+      // Extract className from "class <class_name>"
+      const className = sourceCode.split("class ")[1].split(" ")[0];
 
-
-      console.log({sourceCode, className});
+      console.log({ sourceCode, className });
       return {
         classCode: sourceCode,
         className: className,
